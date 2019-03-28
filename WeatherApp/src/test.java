d61ef921f89d2a6e271d8639c2f8f157dbf0fb05
Austin <duppong_acd@hotmail.com>
2019-03-27 19:31:29 -0500

    Test

--- WeatherApp/src/test.java
+++ WeatherApp/src/test.java
@@ -5,11 +5,12 @@ public class test {
 	public static void main(String[] args) throws JSONException {
 		WeatherHttpRequest request = new WeatherHttpRequest("58504", "us");
 		
+		//url is hardcoded for testing
 		Weather w1 = new Weather(JsonWeatherReader.getWeather(request.getWeatherData()));
 		
-		System.out.println(JsonWeatherReader.getWeather(request.getWeatherData()).location.getCity());
 		System.out.println(w1.location.getCity());
-		
+		System.out.println(w1.location.getSunrise());
+
 
 	}
 
