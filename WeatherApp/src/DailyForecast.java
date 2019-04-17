fe8e64a88971df0236f399b17eecec0cd95cb501
Austin <duppong_acd@hotmail.com>
2019-04-16 19:30:27 -0500

    Date changes
    Changes

--- WeatherApp/src/DailyForecast.java
+++ WeatherApp/src/DailyForecast.java
@@ -1,5 +1,8 @@
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 /**
  * @author duppong
  *
@@ -40,5 +43,21 @@ public class DailyForecast{
 	
     public String getStringDate() {
         return sdf.format(new Date(timestamp));
+    }
+    
+    public Date getDate() throws ParseException
+    {
+    	SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"); 
+		Date date= formatter.parse(this.dateText);
+		return date;
+    }
+    
+    public Calendar getCalendar() throws ParseException
+    {
+    	SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"); 
+		Date date = formatter.parse(this.dateText);
+    	Calendar calendar = Calendar.getInstance();
+		calendar.setTime(date);
+		return calendar;
     }
 }
